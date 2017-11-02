fc94c956369bf2080758674d025bd5398a4152c3
kirfox <muxin.kirill2011@yandex.ru>
2017-11-02 21:30:17 +0300

    create verstka

---
+++ app/src/main/java/com/example/kir/moneytracker/Item.java
@@ -0,0 +1,31 @@
+package com.example.kir.moneytracker;
+
+/**
+ * Created by Kir on 02.11.2017.
+ */
+
+public class Item {
+    private String name;
+    private int price;
+
+    public Item(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
+}
